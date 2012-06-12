dc987bc7a58a0797c64f955336b29e5601e8ca35
Jeremy Mikola <jmikola@gmail.com>
2012-06-12 22:35:02 +0200

    Use TmhOAuth namespace and config array for constructor args

--- src/Kud/Silex/Provider/TmhOAuthServiceProvider.php
+++ src/Kud/Silex/Provider/TmhOAuthServiceProvider.php
@@ -4,22 +4,19 @@ namespace Kud\Silex\Provider;
 
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Themattharris\TmhOAuth;
 
 class TmhOAuthServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        if (!isset($app['tmhoauth.keys.consumer_key']) || !isset($app['tmhoauth.keys.consumer_secret'])) {
-            throw new RuntimeException('You must specify both keys (consumer_key and consumer_secret)');
+        if (!isset($app['tmhoauth.config']['consumer_key'], $app['tmhoauth.config']['consumer_secret'])) {
+            throw \new RuntimeException('Config must include consumer_key and consumer_secret');
         }
 
         $app['tmhoauth'] = $app->share(function () use ($app) {
-            return new tmhOAuth($app['tmhoauth.keys']);
+            return new TmhOAuth($app['tmhoauth.config']);
         });
-
-        if (isset($app['tmhoauth.class_path'])) {
-            $app['autoloader']->registerPrefix('tmhOAuth', $app['tmhoauth.class_path']);
-        }
     }
 
     public function boot(Application $app)
